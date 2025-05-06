63d9533db67a0175e8b93a48509682250961904a
TgTweek <lukassandersen@gmail.com>
2025-05-06 10:05:15 +0200

    Update Program.cs

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -14,7 +14,8 @@ namespace WinFormsApp1
         static void Main()
         {
 
-            string connectionString = "Data Source=SQL6033.site4now.net;Initial Catalog=db_ab68e2_sigurtolsen;User Id=db_ab68e2_sigurtolsen_admin;Password=Admin1234";
+            string connectionString = "Data Source=SQL6032.site4now.net;Initial Catalog=db_ab8928_vet;User Id=db_ab8928_vet_admin;Password=Admin1234";
+
             DbRead = new DbRead(connectionString);
             DbCreate = new DbCreate(connectionString);
 
